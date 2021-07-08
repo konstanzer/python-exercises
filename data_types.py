ba10802a3e700130574ff513c44ffc98035a2853
Steve Newton <newton36@gmail.com>
2021-07-08 14:26:02 -0500

    data types

--- data_types.py
+++ data_types.py
@@ -24,14 +24,53 @@ def can_enroll(class_enrollment, class_slots, student_slots):
 	max_class_size = 21
 	if class_enrollment >= max_class_size:
 		return False
-	for time in class_slots:
-		if student_slots[time] == 1:
+	for s in class_slots:
+		if student_slots[s] == 1:
 			return False
+		else:
+			student_slots[s] == 1
 	return True
+
+#4
+cart_items = 2
+offer_expired = False
+premium = False
+
+def apply_offer(cart_items, offer_expired, premium):
+	if cart_items > 2 and not offer_expired:
+		return True
+	elif premium:
+		return True
+	else:
+		return False
+
+#5
+username = 'codeup'
+password = 'notastrongpassword'
+
+def verify(username, password):
+	verified = np.zeros(4)
+
+	if len(password) >= 5:
+		verified[0] = True
+	if len(username) <= 20:
+		verified[1] = True
+	if username != password:
+		verified[2] = True
+	if username.strip() == username:
+		if password.strip() == password:
+			verified[3] = True
+	
+	return all(verified)
 
 
 if __name__ == "__main__":
-	print("Those movies cost ${0:.2f}".format(total))
+	print("Rentals cost ${0:.2f}".format(total))
 	print("You made ${0:.2f}".format(earnings(rates, hours)))
-	print("Student can enroll: " +
+	print("Student enrolled: " +
 		str(can_enroll(class_enrollment, class_slots, student_slots)))
+	print("Apply offer: " +
+		str(apply_offer(cart_items, offer_expired, premium)))
+	print("Verified: " + str(verify(username, password)))
+
+
