41bc01586abe35742382cd663ceccac61fba2ce7
Steve Newton <newton36@gmail.com>
2021-07-08 17:30:16 -0500

    students file

---
+++ data_structures.py
@@ -0,0 +1,38 @@
+
+"""
+How many students are there?
+How many students prefer light coffee? For each type of coffee roast?
+How many types of each pet are there?
+How many grades does each student have? Do they all have the same number of grades?
+What is each student's grade average?
+How many pets does each student have?
+How many students are in web development? data science?
+What is the average number of pets for students in web development?
+What is the average pet age for students in data science?
+What is most frequent coffee preference for data science students?
+What is the least frequent coffee preference for web development students?
+What is the average grade for students with at least 2 pets?
+How many students have 3 pets?
+What is the average grade for students with 0 pets?
+What is the average grade for web development students? data science students?
+What is the average grade range (i.e. highest grade - lowest grade) for dark coffee drinkers?
+What is the average number of pets for medium coffee drinkers?
+What is the most common type of pet for web development students?
+What is the average name length?
+What is the highest pet age for light coffee drinkers?
+"""
+
+from pprint import pprint
+from students import students
+
+
+
+if __name__ == "__main__":
+    print(pprint(students))
+
+
+
+
+
+
+
