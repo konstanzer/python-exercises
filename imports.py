b8512fbad310a16db1a708a94c3ee766f82a3831
Steve Newton <newton36@gmail.com>
2021-07-14 10:09:47 -0500

    imports

--- imports.py
+++ imports.py
@@ -34,38 +34,40 @@ for u in data:
     else:
         fruits[fruit] = 1
 
-    g = u['greeting']
-    #weird way of returning last number in the string
-    all_unread += int(''.join([c for c in g if c.isnumeric()])[-1])
+    #splits greeting on spaces
+    greeting = list(u['greeting'].split())
+    #unread messages is last number in greeting
+    unread = int([n for n in greeting if n.isnumeric()][-1])
+    all_unread += unread
 
 total_users = active_users + inactive_users
 avg_balance = all_balances / total_users
-# I know
-commonest_fruit =  'strawberry'
-strangest_fruit = 'apple'
+
 
 if __name__ == '__main__':
+
     print(calculate_tip(.1,9800))
     print(grade(99))
 
 	#720 permutations of 6-char string (6!)
+    print("\n6P6")
     print(len(list(itertools.permutations('abc123'))))
     print(factorial(6))
-
+    print("\n4P2 and 4C2")
     #4 choose 2: 4!/2! = 12
     print(len(list(itertools.permutations('abcd',2))))
     #when order doesn't matter: 4!/(2!*2!) = 6
     print(len(list(itertools.combinations('abcd',2))))
 
-    print("\nProfile questions")
+    print("\nProfile questions:")
     print(str(active_users) + " active")
     print(str(inactive_users) + " inactive")
-    print(str(total_users) + " total")
-    print(str(all_balances) + " all")
-    print(str(avg_balance) + " avg")
-    print(str(min_balance) + " min")
-    print(str(max_balance) + " max")
-    print(str(commonest_fruit) + " common fruit")
-    print(str(strangest_fruit) + " rare fruit")
-    print(str(all_unread) + " unread msg")
+    print(str(total_users) + " total users")
+    print('$'+str(all_balances) + " all balances")
+    print('$'+str(avg_balance) + " avg")
+    print('$'+str(min_balance) + " min")
+    print('$'+str(max_balance) + " max")
+    print(str(max(fruits)) + " commonest fruit")
+    print(str(min(fruits)) + " rarest fruit")
+    print(str(all_unread) + " unread messages")
 
